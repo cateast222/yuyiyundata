79e43eeeb12c6fc3df8c8f7f43a9c2891ed29491
dh <dh@yuyiai.com>
2020-05-09 08:18:51 +0800

    3333

--- src/main/webapp/assets/modular/datasource/newspaper/index.js
+++ src/main/webapp/assets/modular/datasource/newspaper/index.js
@@ -1,223 +1,222 @@
-layui.use(['table', 'ax'],
-	function () {
-		var $ = layui.$;
-		var table = layui.table;
-		var $ax = layui.ax;
+layui.use([ 'table', 'ax' ], function() {
+	var $ = layui.$;
+	var table = layui.table;
+	var $ax = layui.ax;
 
-		/**
-		 * 数据源信息表管理
-		 */
-		var Datasource = {
-			tableId: "datasourceTable"
-		};
+	/**
+	 * 数据源信息表管理
+	 */
+	var Datasource = {
+		tableId : "datasourceTable"
+	};
 
-		/**
-		 * 初始化表格
-		 */
-		Datasource.initColumn = function () {
-			return [[
+	/**
+	 * 初始化表格
+	 */
+	Datasource.initColumn = function() {
+		return [ [
 				{
-					type: 'checkbox'
-				}, {
-					title: '序号',
-					type: "numbers"
+					type : 'checkbox'
 				},
 				{
-					field: 'uuid',
-					hide: true,
-					title: "数据源id"
+					title : '序号',
+					type : "numbers"
 				},
 				{
-					field: 'chs_name',
-					sort: true,
-					title: '中文名称',
-//					templet: function (d) {
-//						var url = Feng.ctxPath + '/dataci/newspaper?uuid='
-//							+ d.uuid;
-//						var color = "#01AAED;";
-//						if (d.remark != "" && d.state == 2) {
-//							color = "#FF3333;";
-//						}
-//						return '<a title="备注：' + d.remark
-//							+ '" style="color:' + color
-//							+ '" href="' + url + '">'
-//							+ d.chs_name + '</a>';
-//					},
-					align: 'center',
-					width: 120
+					field : 'uuid',
+					hide : true,
+					title : "数据源id"
 				},
 				{
-					field: 'org_name',
-					sort: true,
-					title: '原名称',
-					templet: function (d) {
-						var url = Feng.ctxPath + '/dataconfig?dataSource=' + d.uuid;
+					field : 'chs_name',
+					sort : true,
+					title : '中文名称',
+					// templet: function (d) {
+					// var url = Feng.ctxPath + '/dataci/newspaper?uuid='
+					// + d.uuid;
+					// var color = "#01AAED;";
+					// if (d.remark != "" && d.state == 2) {
+					// color = "#FF3333;";
+					// }
+					// return '<a title="备注：' + d.remark
+					// + '" style="color:' + color
+					// + '" href="' + url + '">'
+					// + d.chs_name + '</a>';
+					// },
+					align : 'center',
+					width : 120
+				},
+				{
+					field : 'org_name',
+					sort : true,
+					title : '原名称',
+					templet : function(d) {
+						var url = Feng.ctxPath + '/dataconfig?dataSource='
+								+ d.uuid;
 						var color = "#FF3333;";
-						return '<a style="color:' + color + '" href="' + url + '">'	+ d.org_name + '</a>';
+						return '<a style="color:' + color + '" href="' + url
+								+ '">' + d.org_name + '</a>';
 					},
-					align: 'center',
-					width: 120
+					align : 'center',
+					width : 120
 				},
 				{
-					field: 'providerName',
-					sort: true,
-					title: '提供方',
-					align: 'center',
-					width: 140
+					field : 'providerName',
+					sort : true,
+					title : '提供方',
+					align : 'center',
+					width : 140
 				},
 				{
-					field: 'website_url',
-					sort: true,
-					title: '网站地址',
-					templet: function (d) {
-						return '<a title="' + d.website_url +
-							'" style="color: #01AAED;" href="' +
-							d.website_url + '" target="_blank">' +
-							d.website_url + '</a>';
+					field : 'website_url',
+					sort : true,
+					title : '网站地址',
+					templet : function(d) {
+						return '<a title="' + d.website_url
+								+ '" style="color: #01AAED;" href="'
+								+ d.website_url + '" target="_blank">'
+								+ d.website_url + '</a>';
 					}
-				},
-				{
-					field: 'website_name',
-					sort: true,
-					title: '网站名称',
-					align: 'center',
-					width: 120
-				},
-				{
-					field: 'countryName',
-					sort: true,
-					title: '国家',
-					align: 'center',
-					width: 100
 				}, {
-					field: 'region',
-					sort: true,
-					title: '地区'
+					field : 'website_name',
+					sort : true,
+					title : '网站名称',
+					align : 'center',
+					width : 120
 				}, {
-					field: 'proxyName',
-					sort: true,
-					title: '境区',
-					align: 'center',
-					width: 70
+					field : 'countryName',
+					sort : true,
+					title : '国家',
+					align : 'center',
+					width : 100
 				}, {
-					field: 'languageName',
-					sort: true,
-					title: '语种',
-					align: 'center',
-					width: 100
+					field : 'region',
+					sort : true,
+					title : '地区'
 				}, {
-					field: 'encodedName',
-					sort: true,
-					title: '编码',
-					align: 'center',
-					width: 90
+					field : 'proxyName',
+					sort : true,
+					title : '境区',
+					align : 'center',
+					width : 70
 				}, {
-					field: 'stateName',
-					sort: true,
-					title: '状态',
-					align: 'center',
-					width: 65
+					field : 'languageName',
+					sort : true,
+					title : '语种',
+					align : 'center',
+					width : 100
 				}, {
-					toolbar: '#tableBar',
-					title: '操作',
-					align: 'center',
-					width: 130
-				}
-			]];
-		};
+					field : 'encodedName',
+					sort : true,
+					title : '编码',
+					align : 'center',
+					width : 90
+				}, {
+					field : 'stateName',
+					sort : true,
+					title : '状态',
+					align : 'center',
+					width : 65
+				}, {
+					toolbar : '#tableBar',
+					title : '操作',
+					align : 'center',
+					width : 130
+				} ] ];
+	};
 
-		/**
-		 * 点击查询按钮
-		 */
-		Datasource.search = function () {
-			var queryData = {};
-			queryData['condition'] = $("#condition").val();
-			queryData['state'] = $("#state").val();
-			queryData['provider'] = $("#provider").val();
-			table.reload(Datasource.tableId, {
-				where: queryData,
-				page: {
-				    curr: 1 //重新从第 1 页开始
-				  }
-			});
-		};
-
-		// 渲染表格
-		var tableResult = table.render({
-			elem: '#' + Datasource.tableId,
-			url: Feng.ctxPath + '/datasource/list?platform=1',
-			page: true,
-			height: "full-98",
-			cellMinWidth: 100,
-			limit: 15,
-			limits: [15, 30, 50, 100, 200],
-			cols: Datasource.initColumn()
-		});
-
-
-		/**
-		 * 弹出添加对话框
-		 */
-		Datasource.openAddDlg = function () {
-			window.location.href = Feng.ctxPath + '/datasource/addAndEditNewspaper?uuid=';
-		};
-
-		/**
-		 * 点击编辑
-		 * 
-		 * @param data
-		 *            点击按钮时候的行数据
-		 */
-		Datasource.openEditDlg = function (data) {
-			window.location.href = Feng.ctxPath + '/datasource/addAndEditNewspaper?uuid='
-				+ data.uuid;
-		};
-
-		/**
-		 * 点击删除
-		 * 
-		 * @param data
-		 *            点击按钮时候的行数据
-		 */
-		Datasource.onDeleteItem = function (data) {
-			var operation = function () {
-				var ajax = new $ax(Feng.ctxPath + "/datasource/delete",
-					function (data) {
-						Feng.success("删除成功!");
-						table.reload(Datasource.tableId);
-					}, function (data) {
-						Feng.error("删除失败!" + data.responseJSON.message);
-					});
-				ajax.set("uuid", data.uuid);
-				ajax.start();
-			};
-			Feng.confirm("是否删除?", operation);
-		};
-
-		// 搜索按钮点击事件
-		$('#btnSearch').click(function () {
-			Datasource.search();
-		});
-
-		// 添加按钮点击事件
-		$('#btnAdd').click(function () {
-			Datasource.openAddDlg();
-		});
-
-		// 工具条点击事件
-		table.on('tool(' + Datasource.tableId + ')', function (obj) {
-			var data = obj.data;
-			var layEvent = obj.event;
-
-			if (layEvent === 'edit') {
-				Datasource.openEditDlg(data);
-			} else if (layEvent === 'delete') {
-				Datasource.onDeleteItem(data);
+	/**
+	 * 点击查询按钮
+	 */
+	Datasource.search = function() {
+		table.reload(Datasource.tableId, {
+			where : {
+				'condition' : $("#condition").val(),
+				'state' : $("#state").val(),
+				'provider' : $("#provider").val()
+			},
+			page : {
+				curr : 1// 重新从第 1 页开始
 			}
 		});
+	};
 
-		// 添加按钮点击事件
-		$('#btnMigration').click(function () {
-			window.location.href = Feng.ctxPath + '/datasource/migration';
-		});
+	// 渲染表格
+	var tableResult = table.render({
+		elem : '#' + Datasource.tableId,
+		url : Feng.ctxPath + '/datasource/list?platform=1',
+		page : true,
+		height : "full-98",
+		cellMinWidth : 100,
+		limit : 15,
+		limits : [ 15, 30, 50, 100, 200 ],
+		cols : Datasource.initColumn()
 	});
+
+	/**
+	 * 弹出添加对话框
+	 */
+	Datasource.openAddDlg = function() {
+		window.location.href = Feng.ctxPath
+				+ '/datasource/addAndEditNewspaper?uuid=';
+	};
+
+	/**
+	 * 点击编辑
+	 * 
+	 * @param data
+	 *            点击按钮时候的行数据
+	 */
+	Datasource.openEditDlg = function(data) {
+		window.location.href = Feng.ctxPath
+				+ '/datasource/addAndEditNewspaper?uuid=' + data.uuid;
+	};
+
+	/**
+	 * 点击删除
+	 * 
+	 * @param data
+	 *            点击按钮时候的行数据
+	 */
+	Datasource.onDeleteItem = function(data) {
+		var operation = function() {
+			var ajax = new $ax(Feng.ctxPath + "/datasource/delete", function(
+					data) {
+				Feng.success("删除成功!");
+				table.reload(Datasource.tableId);
+			}, function(data) {
+				Feng.error("删除失败!" + data.responseJSON.message);
+			});
+			ajax.set("uuid", data.uuid);
+			ajax.start();
+		};
+		Feng.confirm("是否删除?", operation);
+	};
+
+	// 搜索按钮点击事件
+	$('#btnSearch').click(function() {
+		Datasource.search();
+	});
+
+	// 添加按钮点击事件
+	$('#btnAdd').click(function() {
+		Datasource.openAddDlg();
+	});
+
+	// 工具条点击事件
+	table.on('tool(' + Datasource.tableId + ')', function(obj) {
+		var data = obj.data;
+		var layEvent = obj.event;
+
+		if (layEvent === 'edit') {
+			Datasource.openEditDlg(data);
+		} else if (layEvent === 'delete') {
+			Datasource.onDeleteItem(data);
+		}
+	});
+
+	// 添加按钮点击事件
+	$('#btnMigration').click(function() {
+		window.location.href = Feng.ctxPath + '/datasource/migration';
+	});
+});
